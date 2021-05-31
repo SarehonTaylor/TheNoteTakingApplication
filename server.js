a19435e32a04443a98503f111f75475ec1cc26f5
Sarehon Taylor <sarehontaylor@icloud.com>
2021-05-31 14:56:54 -0400

    fixed typo in the files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 
-const app = express ();]
+const app = express ();
 const PORT = process.env.PORT || 3000;
 
 app.use(expressurl({extended: true}));
 app,use(express.json());
-app.use(expres,static(path.join(__dirname, './public')))let
+app.use(expres,static(path.join(__dirname, './public')))
 
 require('./public/assets')
